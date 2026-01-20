c46b3829afd883396a29265cd7e7e6b71ef0a83c
dev-priyanshipatel <priyanshipatel8304@gmail.com>
2026-01-20 09:55:10 +0530

    updated Offcanvas

--- src/components/custom/Toparrow.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Toparrow = () => {
-  return (
-    <div>Toparrow</div>
-  )
-}
-
-export default Toparrow
--- src/components/react-bootstrap/offCanvas.jsx
+++ src/components/react-bootstrap/offCanvas.jsx
@@ -18,7 +18,7 @@ const OffCanvas = () => {
       </Button>
 
       <Offcanvas show={show} onHide={handleClose}>
-        <Offcanvas.Header closeButton>
+        <Offcanvas.Header closeButton className='border-bottom'>
           <Offcanvas.Title>
             <Logo />
           </Offcanvas.Title>
@@ -26,7 +26,7 @@ const OffCanvas = () => {
         <Offcanvas.Body>
           <nav>
             <ul className="navbar-nav offcanvas-navbar flex-row gap-3 m-0">
-              <li className="nav-item">
+              <li className="nav-item border-bottom">
                 <a
                   href="#"
                   className="nav-link p-3 text-decoration-none text-dark fw-medium facilities"
@@ -34,7 +34,7 @@ const OffCanvas = () => {
                   Listing<i className="ri-arrow-down-s-line"></i>
                 </a>
               </li>
-              <li className="nav-item">
+              <li className="nav-item border-bottom">
                 <a
                   href="#"
                   className=" nav-link p-3 text-decoration-none text-dark  fw-medium facilities"
@@ -42,7 +42,7 @@ const OffCanvas = () => {
                   Pages<i className="ri-arrow-down-s-line"></i>
                 </a>
               </li>
-              <li className="nav-item">
+              <li className="nav-item border-bottom">
                 <a
                   href="#"
                   className=" nav-link p-3 text-decoration-none text-dark  fw-medium facilities"
@@ -52,42 +52,7 @@ const OffCanvas = () => {
               </li>
             </ul>
           </nav>
-          <div className="d-flex align-items-center">
-            <ul className="navbar-nav flex-column gap-1 m-0 d-block">
-              <li className="nav-item bg-color rounded-4  px-3 py-2">
-                <a
-                  href="#"
-                  className=" nav-link text-decoration-none text-dark  fw-medium facilities"
-                >
-                  <i className="ri-hotel-line fs-5 me-2"></i>Hotel
-                </a>
-              </li>
-              <li className="nav-item bg-color rounded-4  px-3 py-2">
-                <a
-                  href="#"
-                  className=" nav-link text-decoration-none text-dark  fw-medium facilities"
-                >
-                  <i className="ri-plane-line fs-5 me-2"></i>Flight
-                </a>
-              </li>
-              <li className="nav-item bg-color rounded-4  px-3 py-2">
-                <a
-                  href="#"
-                  className=" nav-link text-decoration-none text-dark  fw-medium facilities"
-                >
-                  <i className="ri-global-line fs-5 me-2"></i>Tour
-                </a>
-              </li>
-              <li className="nav-item bg-color rounded-4  px-3 py-2">
-                <a
-                  href="#"
-                  className=" nav-link text-decoration-none text-dark  fw-medium facilities"
-                >
-                  <i className="ri-car-line fs-5 me-2"></i>Cab
-                </a>
-              </li>
-            </ul>
-          </div>
+          
         </Offcanvas.Body>
       </Offcanvas>
     </>
